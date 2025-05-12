f7716ba21dabeaca8fbbe0c40e5fc1c64b006dcb
Moncks <ggmoncks@gmail.com>
2025-05-12 12:02:32 -0300

    feat: Refactor Usuario class to include additional user attributes in login and logout methods

--- utils/classes/user.py
+++ utils/classes/user.py
@@ -1,31 +1,43 @@
 
-import time
 
 class Usuario:
 
     def __init__(self):
         self.id_usuario = None
+        self.dt_cadastro = None
         self.nome = None
         self.senha = None
-        self.logado = False
-        # self.permissao = None
-        self.ultimo_login = None
+        self.score = None
+        self.admin = None
+        self.dt_ultimo_acesso = None
 
-    def login(self, id_usuario, nome, senha):
+        self.logado = False
+
+    def login(self, id_usuario, dt_cadastro, nome, senha, score, admin, dt_ultimo_acesso):
         self.id_usuario = id_usuario
+        self.dt_cadastro = dt_cadastro
         self.nome = nome
         self.senha = senha
+        self.score = score
+        self.admin = admin
+        self.dt_ultimo_acesso = dt_ultimo_acesso
+
+
         self.logado = True
-        # self.permissao = "admin"
-        self.ultimo_login = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
     
     def logout(self):
         self.id_usuario = None
+        self.dt_cadastro = None
         self.nome = None
         self.senha = None
+        self.score = None
+        self.admin = None
+        self.dt_ultimo_acesso = None
+
         self.logado = False
-        # self.permissao = None
-        self.ultimo_login = None
+
+
 
 
 
