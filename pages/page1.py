2dbe9004d3164cecc4bf8ee685c20ea8f8024096
Moncks <ggmoncks@gmail.com>
2025-05-12 12:03:55 -0300

    feat: Implement user information display on page1 with dynamic content

--- pages/page1.py
+++ pages/page1.py
@@ -4,7 +4,10 @@ from dash import (
 import dash_bootstrap_components as dbc
 import time
 
-
+from main import (
+    db_comm,
+    usuario_logado
+)
 
 
 register_page(
@@ -14,6 +17,26 @@ register_page(
     name="page1",
 )
 
-layout = html.Div("Page 1")
+layout = dbc.Container(
+    id="page1-container",
+    children = "Page 1",
+    )
 
+@callback(
+    Output("page1-container", "children"),
+    Input("url", "pathname"),
+)
+def callback_atualizar_page1(pathname):
+    if pathname == "/page1":
+        return [
+            html.H1("Martelo Amigo"),
+            html.Hr(),
+            html.H4("Informações do usuário"),
+            html.P(f"Usuário logado: {usuario_logado.nome.title()}"),  
+            html.P(f"Tipo de acesso: {"Administrador" if usuario_logado.admin else "Morador"}"),	
+            html.P(f"Data do último acesso: {usuario_logado.dt_ultimo_acesso if usuario_logado.dt_ultimo_acesso else 'Nunca'}"),
+            html.P(f"Data de cadastro: {usuario_logado.dt_cadastro}"),
+            html.P(f"ID do usuário: {usuario_logado.id_usuario}"),
+            html.P(f"Score do usuário: {usuario_logado.score}"),
+        ]
     
