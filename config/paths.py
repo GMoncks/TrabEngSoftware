485a1b3202df1f9d3d32a4bebc2a623bb1803e68
Moncks <ggmoncks@gmail.com>
2025-05-12 10:42:11 -0300

    feat: Add paths configuration for database and utilities

---
+++ config/paths.py
@@ -0,0 +1,9 @@
+import os
+
+# Path do repositorio (fora da pasta atual)
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+UTILS_FOLDER = os.path.join(BASE_PATH, 'utils')
+
+DATABASE_FOLDER = os.path.join(UTILS_FOLDER, 'database')
+DATABASE_PATH = os.path.join(DATABASE_FOLDER, 'sqlite_database.db')
