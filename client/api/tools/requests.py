948b0bb435f2fc6c67b41931eb72703f0c57f57a
Moncks <ggmoncks@gmail.com>
2025-06-22 22:29:24 -0300

    feat: update consultar_ferramentas method to accept id_dono search

--- client/api/tools/requests.py
+++ client/api/tools/requests.py
@@ -9,7 +9,7 @@ class ToolRequest:
         self.editar_ferramenta = '/ferramentas/editar'
         self.request = request_helper(baseURL)
 
-    def consultar_ferramentas(self, nome, id_categoria, data_emprestimo, data_devolucao):
+    def consultar_ferramentas(self, nome=None, id_categoria=None, data_emprestimo=None, data_devolucao=None, id_dono=None):
         """
         Consulta a API de ferramentas por nome e disponibilidade
         """
@@ -23,6 +23,8 @@ class ToolRequest:
             consulta['data_emprestimo'] = data_emprestimo
         if data_devolucao:
             consulta['data_devolucao'] = data_devolucao
+        if id_dono:
+            consulta['id_usuario'] = id_dono
         
         return self.request.get(self.buscar_ferramentas, consulta)
     
